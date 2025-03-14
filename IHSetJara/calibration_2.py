cf7614a0d04d61cbf199b19605301abf8581f14d
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-03-14 13:49:20 +0100

    naming model

--- IHSetJara/calibration_2.py
+++ IHSetJara/calibration_2.py
@@ -306,12 +306,12 @@ class cal_Jara_2(object):
         self.full_run = self.run_model(self.solution)
 
         if self.switch_Yini == 0:
-            self.par_names = [r'$C^+$', r'$C^-$']
+            self.par_names = [r'C+', r'C-']
             self.par_values = self.solution.copy()
             self.par_values[0] = -np.exp(self.par_values[0])
             self.par_values[1] = -np.exp(self.par_values[1])
         elif self.switch_Yini == 1:
-            self.par_names = [r'$C^+$', r'$C^-$', r'$Y_{i}$']
+            self.par_names = [r'C+', r'C-', r'Y_{i}']
             self.par_values = self.solution.copy()
             self.par_values[0] = -np.exp(self.par_values[0])
             self.par_values[1] = -np.exp(self.par_values[1])
--- IHSetJara/direct_run.py
+++ IHSetJara/direct_run.py
@@ -181,10 +181,10 @@ class Jara_run(object):
     def run(self, par):
         self.full_run = self.run_model(par)
         if self.switch_Yini == 1:
-            self.par_names = [r'$C^+$', r'$C^-$']
+            self.par_names = [r'C+', r'C-']
             self.par_values = par
         elif self.switch_Yini == 0:
-            self.par_names = [r'$C^+$', r'$C^-$', r'$Y_{i}$']
+            self.par_names = [r'C+', r'C-', r'Y_{i}']
             self.par_values = par
         
         self.calculate_metrics()
