3826ae8861e16402f4c2a7ad3359ace19ccc09e6
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-05-31 10:50:54 +0200

    Correcting writer bug

--- IHSetJara/calibration_2.py
+++ IHSetJara/calibration_2.py
@@ -269,16 +269,16 @@ class cal_Jara_2(object):
         self.Hcr = self.Hcr[ii:]
         self.time = self.time[ii:]
         
-        idx = np.where((self.time < self.start_date) | (self.time > self.end_date))
+        idx = np.where((self.time < self.start_date) | (self.time > self.end_date))[0]
         self.idx_validation = idx
 
-        idx = np.where((self.time >= self.start_date) & (self.time <= self.end_date))
+        idx = np.where((self.time >= self.start_date) & (self.time <= self.end_date))[0]
         self.idx_calibration = idx
         self.hb_splited = self.hb[idx]
         self.Hcr_splited = self.Hcr[idx]
         self.time_splited = self.time[idx]
 
-        idx = np.where((self.time_obs >= self.start_date) & (self.time_obs <= self.end_date))
+        idx = np.where((self.time_obs >= self.start_date) & (self.time_obs <= self.end_date))[0]
         self.Obs_splited = self.Obs[idx]
         self.time_obs_splited = self.time_obs[idx]
 
