f59aabdee2880a0eb9b79ff3f15eba51e0ae0f66
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-06-14 12:56:58 +0200

    Correcting angles and depth bug

--- IHSetJara/calibration_2.py
+++ IHSetJara/calibration_2.py
@@ -34,9 +34,7 @@ class cal_Jara_2(object):
         self.switch_Yini = cfg['switch_Yini']
         self.switch_brk = cfg['switch_brk']
         if self.switch_brk == 1:
-            self.bathy_angle = cfg['bathy_angle']
             self.breakType = cfg['break_type']
-            self.depth = cfg['depth']
         self.doc_formula = cfg['doc_formula']
         self.xc = cfg['xc']
         self.Hberm = cfg['Hberm']
@@ -56,6 +54,8 @@ class cal_Jara_2(object):
             self.Obs = self.Obs[~data.mask_nan_average_obs]
             self.time_obs = pd.to_datetime(data.time_obs.values)
             self.time_obs = self.time_obs[~data.mask_nan_average_obs]
+            self.depth = np.mean(data.waves_depth.values)
+            self.bathy_angle = circmean(data.phi.values, high=360, low=0)
         else:
             self.hs = data.hs.values[:, cfg['trs']]
             self.tp = data.tp.values[:, cfg['trs']]
@@ -65,6 +65,8 @@ class cal_Jara_2(object):
             self.Obs = self.Obs[~data.mask_nan_obs[:, cfg['trs']]]
             self.time_obs = pd.to_datetime(data.time_obs.values)
             self.time_obs = self.time_obs[~data.mask_nan_obs[:, cfg['trs']]]
+            self.depth = data.waves_depth.values[cfg['trs']]
+            self.bathy_angle = data.phi.values[cfg['trs']]
         
         self.start_date = pd.to_datetime(cfg['start_date'])
         self.end_date = pd.to_datetime(cfg['end_date'])
