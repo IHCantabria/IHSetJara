3bd412cb54c84d794ddee9ababb0cfc2b508d623
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-07-24 10:08:48 +0200

    trying new superclass

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='IHSetJara',
-    version='1.1.9',
+    version='1.1.10',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'numpy',
         'xarray',
         'numba',
+        'IHSetUtils @ git+https://github.com/IHCantabria/IHSetUtils.git',
         'fast_optimization @ git+https://github.com/defreitasL/fast_optimization.git'
     ],
     author='Changbin Lim',
